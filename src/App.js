64c5bca83179a248157db2afdb3a24c493c0b6e2
Vladyslav Bochkovskyi <bochkovskyi.vladyslav@gmail.com>
2021-07-21 14:15:14 +0300

    Test222

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react';
 import Profile from './components/profile/Profile';
 import StatsList from './components/stats/StatsList';
 import FriendsList from './components/friends/FriendsList';
